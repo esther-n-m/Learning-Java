4f32edde2334ebb8281a85fb44d5783fe4e3bf63
Esther <esthermutembei50@gmail.com>
2026-09-13 17:53:24 +0300

    Add variables in Java language

--- Main.java
+++ Main.java
@@ -2,5 +2,26 @@ public class Main {
     public static void main (String [] args) {
         System.out.println("Esther is my name.");
         System.out.println(12*2);
+        //Print a name
+        System.out.println("Joe");
+        /*
+        Adding a multi-line comment
+        */
+
+        //Variables
+        String myName = "Esie";
+        System.out.println(myName);
+
+        int myNum = 123;
+        System.out.println(myNum);
+
+        float myFloatNum = 4.55f;
+        System.out.println(myFloatNum);
+
+        char myLetter = 'E';
+        System.out.println(myLetter);
+
+        boolean myBool = true;
+        System.out.println(myBool);
     }
 }
